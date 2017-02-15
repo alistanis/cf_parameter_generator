8dcea7d0c5588a5905c1a868bcabd8e48ea87a43
Chris Cooper <christopher_cooper@harvard.edu>
2017-02-15 18:47:27 -0500

    adds options for formatting

--- main.go
+++ main.go
@@ -2,21 +2,28 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"sort"
+	"strings"
 )
 
 var (
-	inFile  string
-	outFile string
+	inFile          string
+	outFile         string
+	min             bool
+	numIndentSpaces int
 )
 
 func init() {
-	flag.StringVar(&inFile, "f", "", "The file to read from to generate parameters")
-	flag.StringVar(&outFile, "o", "", "Optional: Specify a file name to write out parameters")
+	flag.StringVar(&inFile, "f", "", "The file to read from to generate parameters.")
+	flag.StringVar(&outFile, "o", "", "Optional: Specify a file name to write out parameters.")
+	flag.BoolVar(&min, "min", false, "If given, will generate minified output.")
+	flag.IntVar(&numIndentSpaces, "spaces", 2, "The number of spaces used to indent the file if not generating minified output.")
+
 }
 
 func main() {
@@ -29,52 +36,70 @@ func main() {
 	m := make(map[string]interface{})
 	data, err := ioutil.ReadFile(inFile)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(-1)
+		exitError(err)
 	}
 	err = json.Unmarshal(data, &m)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(-1)
+		exitError(err)
 	}
 
 	if _, ok := m["Parameters"]; !ok {
-		fmt.Fprintln(os.Stderr, "Parameters not found in file")
-		os.Exit(-1)
+		exitError(errors.New("Parameters not found in file."))
 	}
 
 	params := m["Parameters"].(map[string]interface{})
 
-	keys := make([]string, 0)
+	pl := make(paramList, 0)
 
 	for k := range params {
-		keys = append(keys, k)
+		p := parameter{ParameterKey: k}
+		pl = append(pl, p)
 	}
 
-	sort.Strings(keys)
+	sort.Sort(pl)
 
-	s := "[\n"
-
-	for i, k := range keys {
-		s += fmt.Sprintf(`	{
-		"ParameterKey": "%s",
-		"ParameterValue": ""
-	}`, k)
-		if i != len(keys)-1 {
-			s += `,
-`
-		}
+	data, err = marshal(pl)
+	if err != nil {
+		exitError(err)
 	}
-
-	s += "\n]"
-
 	if outFile != "" {
-		err = ioutil.WriteFile(outFile, []byte(s), 0644)
+		err = ioutil.WriteFile(outFile, data, 0644)
 		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
+			exitError(err)
 		}
 	} else {
-		fmt.Print(s)
+		fmt.Print(string(data))
 	}
 
+}
+
+func marshal(i interface{}) ([]byte, error) {
+	if min {
+		return json.Marshal(i)
+	}
+	return json.MarshalIndent(i, "", strings.Repeat(" ", numIndentSpaces))
+}
+
+type parameter struct {
+	ParameterKey   string
+	ParameterValue string
+}
+
+type paramList []parameter
+
+func (p paramList) Less(i, j int) bool {
+	return p[i].ParameterKey < p[j].ParameterKey
+}
+
+func (p paramList) Swap(i, j int) {
+	p[i], p[j] = p[j], p[i]
+}
+
+func (p paramList) Len() int {
+	return len(p)
+}
+
+func exitError(err error) {
+	fmt.Fprintln(os.Stderr, err)
+	os.Exit(-1)
 }
